7147f785e41c6bd6b1f83f6a1fa6e09c4ba5b83c
DESKTOP-P3D43EL\PC-07 <lsa4449@naver.com>
2020-08-31 20:37:54 +0900

    운동법 등록 수정 삭제 구현 !

--- src/main/common/kr/or/ddit/vo/HealthImageVO.java
+++ src/main/common/kr/or/ddit/vo/HealthImageVO.java
@@ -7,14 +7,31 @@ public class HealthImageVO {
 	private String healthImage_no; // 헬스 번호
 	private String healthImage_title; // 헬스 제목
 	private String healthImage_content; // 헬스 내용
+	private String healthImage_category; // 헬스 카테고리
 	private String healthImage_age; // 헬스 추천 나이
 	private String healthImage_diet; // 헬스 운동법(유,무산소)
-	private String healthImage_tiem; // 헬스 시간
+	private String healthImage_time; // 헬스 시간
+	private String healthImage_difficulty; // 헬스 난이도
 	private int rnum; // 순서
 	private String mem_no; // 멤버 번호
 	private List<HealthFileVO> items; // 이미지 리스트
 
-	
+	public String getHealthImage_category() {
+		return healthImage_category;
+	}
+
+	public void setHealthImage_category(String healthImage_category) {
+		this.healthImage_category = healthImage_category;
+	}
+
+	public String getHealthImage_difficulty() {
+		return healthImage_difficulty;
+	}
+
+	public void setHealthImage_difficulty(String healthImage_difficulty) {
+		this.healthImage_difficulty = healthImage_difficulty;
+	}
+
 	public String getHealthImage_age() {
 		return healthImage_age;
 	}
@@ -31,12 +48,12 @@ public class HealthImageVO {
 		this.healthImage_diet = healthImage_diet;
 	}
 
-	public String getHealthImage_tiem() {
-		return healthImage_tiem;
+	public String getHealthImage_time() {
+		return healthImage_time;
 	}
 
-	public void setHealthImage_tiem(String healthImage_tiem) {
-		this.healthImage_tiem = healthImage_tiem;
+	public void setHealthImage_time(String healthImage_time) {
+		this.healthImage_time = healthImage_time;
 	}
 
 	public int getRnum() {
--- src/main/java/kr/or/ddit/healthImage/controller/HealthImageController.java
+++ src/main/java/kr/or/ddit/healthImage/controller/HealthImageController.java
@@ -5,12 +5,16 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.or.ddit.healthImage.service.IHealthImageService;
 import kr.or.ddit.healthImageFile.service.IHealthImageFileService;
 import kr.or.ddit.vo.HealthImageVO;
+import kr.or.ddit.vo.KnowledgeVO;
 
 @Controller
 @RequestMapping("/admin/healthImage/")
@@ -21,8 +25,8 @@ public class HealthImageController {
 	@Autowired
 	private IHealthImageFileService healthFileService;
 	
+	// 운동법 리스트
 	@RequestMapping("healthImageList")
-	
 	public ModelAndView healthImageList(ModelAndView andView, Map<String, String> params) throws Exception {
 
 		List<HealthImageVO> healthImageList = this.healthImageService.healthList(params);
@@ -31,7 +35,67 @@ public class HealthImageController {
 		andView.setViewName("admin/healthImage/healthImageList");
 
 		return andView;
-
 	}
+	
+	@RequestMapping("chooseList")
+	public ModelAndView chooseList(ModelAndView andView, Map<String, String> params, @RequestParam(value = "choose") String choose) throws Exception{
+		System.out.println(choose);
+		
+		params.put("healthImage_category", choose);
+		
+		List<HealthImageVO> healthImageList = this.healthImageService.healthList(params);
+		
+		andView.addObject("healthImageList", healthImageList);
+		andView.setViewName("admin/healthImage/healthImageList");
+		
+		return andView;
+	}
+	
+	// 운동법 폼
+	@RequestMapping("healthImageForm")
+	public void healthImageForm() {
+		
+	}
+	
+	// 운동법  등록
+	@RequestMapping("inserthealthImageInfo")
+	public String inserthealthImageInfo(HealthImageVO healthInfo, @RequestParam("files") MultipartFile[] items)
+			throws Exception {
+		this.healthImageService.insertHealth(healthInfo, items);
 
+		return "redirect:/admin/healthImage/healthImageList.do";
+	}
+	
+	// 운동법 삭제
+		@RequestMapping("deleteHealthImageInfo")
+		public String deleteHealthImageInfo(@RequestParam(value = "healthImage_no") String healthImage_no) throws Exception {
+
+			this.healthImageService.deleteHealth(healthImage_no);
+
+			return "redirect:/admin/healthImage/healthImageList.do";
+		}
+
+	
+	// 운동법 상세
+	@RequestMapping("healthImageView")
+	public HealthImageVO healthImageView(@RequestParam(value = "healthImage_no") String healthImage_no, 
+											Map<String, String> params, 
+											Model model,
+											HealthImageVO healthInfo) throws Exception {
+		params.put("healthImage_no", healthImage_no);
+		
+		healthInfo = this.healthImageService.healthInfo(params);
+		model.addAttribute("healthInfo", healthInfo);
+
+		return healthInfo;
+	}
+	
+	// 운동법 수정
+		@RequestMapping("updateHealthImageInfo")
+		public String updateKnowledge(HealthImageVO healthInfo, @RequestParam("files") MultipartFile[] items)
+				throws Exception {
+			this.healthImageService.updateHealth(healthInfo, items);
+
+			return "redirect:/admin/healthImage/healthImageList.do";
+		}
 }
